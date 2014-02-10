5b3e442acd1f5ba37dfd3ede1a2798ad376e9b86
Samuel DiBella <samuel.dibella@gmail.com>
2014-02-10 18:58:03 -0500

    rewrote move code to use translate.forward instead of GetAxis commands. "Flying" bug is still present whenever "W" or "S" is pressed.

--- Assets/scripts/player_script.cs
+++ Assets/scripts/player_script.cs
@@ -1,64 +1,47 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
-
-
-//script governing player movement
 public class player_script : MonoBehaviour {
-	//initialized vars
-	float speed = 10.0f;
-	float jumpSpeed = 100f;
-	float rotationSpeed = 100.0f;
-	float gravity = 7.5f;
+	float speed = 20f;
+	float jumpSpeed = 300f;
+	float gravity = 20f;
+	float rotateSpeed = 5;
 	Vector3 moveDirection;
-	int jumpTurns;
-	int airTime;
-
+	int jumpTurns = 15;
 	// Update is called once per frame
 	void Update () {
-	
-		playerMove ();
-
-		//if block creates jump behavior when space bar is pressed. upward force 
-		//is applied, and decreases, over several moments. stores y-axis movement in
-		//a Vector3
-		if (GetComponent<CharacterController>().isGrounded && Input.GetKeyDown (KeyCode.Space)) {
+		CharacterController ctrl = GetComponent<CharacterController>();
+		
+		
+		if(Input.GetKey(KeyCode.W)) {
+			moveDirection = transform.forward * speed * Time.deltaTime;
+		}
+		
+		if(Input.GetKey(KeyCode.A)) {
+			ctrl.transform.Rotate(0, -rotateSpeed, 0);
+		}
+		
+		if(Input.GetKey(KeyCode.S)) {
+			moveDirection = transform.forward * -speed * Time.deltaTime;
+		}
+		
+		if(Input.GetKey(KeyCode.D)) {
+			ctrl.transform.Rotate(0,rotateSpeed,0);
+		}
+		
+		if(Input.GetKey(KeyCode.Space) && ctrl.isGrounded) {	
 			jumpTurns = 15;
-		} else if (jumpTurns > 0) {
-			moveDirection.y = (jumpSpeed/(16 - jumpTurns));
+		}
+		
+		if(jumpTurns > 0) {
+			moveDirection.y = jumpSpeed/(16 - jumpTurns);
+			moveDirection.y *= Time.deltaTime;
 			jumpTurns--;
 		}
-
-		if (!GetComponent<CharacterController> ().isGrounded && airTime < 100) {
-						airTime++;
-		} else if (airTime == 30) {
-			GetComponent<CharacterController> ().center = new Vector3(830.9f,56.03f,505.6494f);
-		} else {
-			airTime = 0;		
-		}
-		//applies gravity to above ^ Vector3
-		moveDirection.y = moveDirection.y - gravity;
-		moveDirection.y *= Time.deltaTime;
-		GetComponent<CharacterController>().Move(moveDirection);
-	}	
 		
-	//moves player from wasd input. 
-	//based on code from Unity GetAxis documentation
-	void playerMove() {
-		// Get the horizontal and vertical axis.
-		// By default they are mapped to the arrow keys.
-		// The value is in the range -1 to 1
-		float translation = Input.GetAxis ("Vertical") * speed;
-		float rotation = Input.GetAxis ("Horizontal") * rotationSpeed;
-		
-		// Make it move 10 meters per second instead of 10 meters per frame...
-		translation *= Time.deltaTime;
-		rotation *= Time.deltaTime;
-		
-		// Move translation along the object's z-axis
-		transform.Translate (0, 0, translation);
-		// Rotate around our y-axis
-		transform.Rotate (0, rotation, 0);
-
+		moveDirection.y -= gravity * Time.deltaTime;
+		ctrl.Move(moveDirection);
+		moveDirection.z = 0;
+		moveDirection.x = 0;
 	}
 }
